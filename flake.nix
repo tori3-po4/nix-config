136d072e0cc6678bbfde9d5d1a24b71ad6b4c0fa
toriyamaiori <iori.wakuwaku.programing@gmail.com>
2026-05-13 18:05:48 +0900

    flakeをマルチシステムに変えた

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "macOS configuration for your-host";
+  description = "Iori's Nix configuration (darwin / nixos / home-manager)";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -22,39 +22,86 @@
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
   };
 
-  outputs = inputs@{ self, nixpkgs, nix-darwin, home-manager, nix-vscode-extensions, nix-homebrew }:
+  outputs = inputs@{ self, nixpkgs, nix-darwin, home-manager, nix-vscode-extensions, nix-homebrew, ... }:
   let
-    username = "yourname";
-    hostname = "your-host";
-    system   = "aarch64-darwin";
-  in {
-    darwinConfigurations.${hostname} = nix-darwin.lib.darwinSystem {
-      inherit system;
-      specialArgs = { inherit inputs username; };
-      modules = [
-        ./darwin
-        {
-          nixpkgs.overlays = [ nix-vscode-extensions.overlays.default ];
-          nixpkgs.config.allowUnfree = true;
-        }
-        nix-homebrew.darwinModules.nix-homebrew
-        {
-          nix-homebrew = {
-            enable = true;
-            enableRosetta = true;
-            user = username;
-            autoMigrate = true;  # 既存の手動インストールHomebrewを引き継ぐ
-          };
-        }
-        home-manager.darwinModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.backupFileExtension = "hmbak";
-          home-manager.users.${username} = import ./home;
-          home-manager.extraSpecialArgs = { inherit inputs username; };
-        }
-      ];
+    # 想定対応システム: aarch64-darwin / x86_64-linux / aarch64-linux
+    # (実ホストは下の *Configurations で個別に紐づける)
+
+    # 共通 nixpkgs 設定 (overlay + unfree)
+    sharedNixpkgsModule = {
+      nixpkgs.overlays = [ nix-vscode-extensions.overlays.default ];
+      nixpkgs.config.allowUnfree = true;
     };
+
+    # home-manager を system モジュールとして組み込むときの共通設定
+    homeManagerSharedModule = { username }: {
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      home-manager.backupFileExtension = "hmbak";
+      home-manager.users.${username} = import ./home;
+      home-manager.extraSpecialArgs = { inherit inputs username; };
+    };
+
+    # darwinConfigurations のアトリビュート名がホスト名として使われる
+    mkDarwin = { username, system }:
+      nix-darwin.lib.darwinSystem {
+        inherit system;
+        specialArgs = { inherit inputs username; };
+        modules = [
+          ./darwin
+          sharedNixpkgsModule
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              enable = true;
+              enableRosetta = true;
+              user = username;
+              autoMigrate = true; # 既存の手動インストール Homebrew を引き継ぐ
+            };
+          }
+          home-manager.darwinModules.home-manager
+          (homeManagerSharedModule { inherit username; })
+        ];
+      };
+  in {
+    darwinConfigurations = {
+      "your-host" = mkDarwin {
+        username = "yourname";
+        system   = "aarch64-darwin";
+      };
+    };
+
+    # ---------------------------------------------------------------------
+    # 以下は Linux ホストを追加するときの足場 (現状は空)。
+    #
+    # NixOS そのものを管理する場合は nixosConfigurations を使う:
+    #
+    #   nixosConfigurations."<host>" = nixpkgs.lib.nixosSystem {
+    #     system = "x86_64-linux"; # or "aarch64-linux"
+    #     specialArgs = { inherit inputs; username = "<user>"; };
+    #     modules = [
+    #       ./nixos                                    # 別途作成
+    #       sharedNixpkgsModule
+    #       home-manager.nixosModules.home-manager
+    #       (homeManagerSharedModule { username = "<user>"; })
+    #     ];
+    #   };
+    #
+    # NixOS 以外の Linux (Ubuntu/Arch 等) に Nix だけ入れて home-manager を
+    # 使う場合は standalone home-manager:
+    #
+    #   homeConfigurations."<user>@<host>" = home-manager.lib.homeManagerConfiguration {
+    #     pkgs = import nixpkgs {
+    #       system = "x86_64-linux";
+    #       config.allowUnfree = true;
+    #       overlays = [ nix-vscode-extensions.overlays.default ];
+    #     };
+    #     extraSpecialArgs = { inherit inputs; username = "<user>"; };
+    #     modules = [ ./home ];
+    #   };
+    # ---------------------------------------------------------------------
+
+    nixosConfigurations = { };
+    homeConfigurations  = { };
   };
 }
