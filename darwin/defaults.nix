7d8db17ef4b804b401837ebbf96cebfcfa3b4c5b
toriyamaiori <iori.wakuwaku.programing@gmail.com>
2026-05-15 21:33:47 +0900

    ライトモードを強制するようにした

--- darwin/defaults.nix
+++ darwin/defaults.nix
@@ -18,7 +18,7 @@
 
     NSGlobalDomain = {
       AppleEnableSwipeNavigateWithScrolls = false;
-      AppleInterfaceStyle = "Light";
+      AppleInterfaceStyle = null;
       AppleInterfaceStyleSwitchesAutomatically = false;
       AppleShowAllExtensions = true;
       AppleSpacesSwitchOnActivate = true;
