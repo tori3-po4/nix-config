3e62c49b09e43df6325bc48c9a6de1e4c60db320
toriyamaiori <iori.wakuwaku.programing@gmail.com>
2026-07-11 15:03:11 +0900

    bitwardenの設定を変更した

--- darwin/bitwarden.nix
+++ darwin/bitwarden.nix
@@ -1,6 +1,13 @@
 { username, ... }:
+let
+  sshAuthSock = "/Users/${username}/.bitwarden-ssh-agent.sock";
+in
 {
+  environment.variables = {
+    SSH_AUTH_SOCK = sshAuthSock;
+  };
+
   launchd.user.envVariables = {
-    SSH_AUTH_SOCK = "/Users/${username}/.bitwarden-ssh-agent.sock";
+    SSH_AUTH_SOCK = sshAuthSock;
   };
 }
