1bd7b1aee0107a25d1b88a84f95e92eee331b5ab
toriyamaiori <iori.wakuwaku.programing@gmail.com>
2026-07-19 00:35:45 +0900

    add dns settings

--- darwin/defaults.nix
+++ darwin/defaults.nix
@@ -115,4 +115,17 @@
       };
     };
   };
+  networking = {
+    knownNetworkServices = [
+      "Wi-Fi"
+      "Thunderbolt Ethernet"
+    ];
+
+    dns = [
+      "1.1.1.1"
+      "1.0.0.1"
+      "2606:4700:4700::1111"
+      "2606:4700:4700::1001"
+    ];
+  };
 }
