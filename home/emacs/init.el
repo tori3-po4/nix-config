24557b56f78f588616b4677262eb68fe6b05f015
toriyamaiori <iori.wakuwaku.programing@gmail.com>
2026-09-02 17:39:58 +0900

    enable menu-bar and tool-bar

--- home/emacs/init.el
+++ home/emacs/init.el
@@ -60,9 +60,9 @@
         tab-always-indent 'complete
         display-line-numbers-width-start t)
 (setq-default truncate-lines t)
-(menu-bar-mode -1)
+(menu-bar-mode 1)
 (when (fboundp 'tool-bar-mode)
-  (tool-bar-mode -1))
+  (tool-bar-mode 1))
 (when (fboundp 'scroll-bar-mode)
   (scroll-bar-mode -1))
 (column-number-mode 1)
