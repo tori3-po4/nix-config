b0040d1ec08c9f3edcc3ff71d4ce4f851c99018b
tori3-por4 <88425634+tori3-po4@users.noreply.github.com>
2026-09-07 22:28:29 +0900

    add ssh agent setting for linux

--- linux/default.nix
+++ linux/default.nix
@@ -1,5 +1,10 @@
+{ config, ... }:
 {
   imports = [
     ./flatpak.nix
   ];
+
+  # Flatpak版BitwardenのSSHエージェントを利用する。
+  home.sessionVariables.SSH_AUTH_SOCK =
+    "${config.home.homeDirectory}/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock";
 }
