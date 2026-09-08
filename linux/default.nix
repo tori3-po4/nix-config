d7bb3fa4a07a79b3fe9b1421d9306198e6f581d8
tori3-por4 <88425634+tori3-po4@users.noreply.github.com>
2026-09-09 02:41:22 +0900

    set emacs-daemon

--- linux/default.nix
+++ linux/default.nix
@@ -14,6 +14,19 @@ in
   # Flatpak版BitwardenのSSHエージェントを利用する。
   home.sessionVariables.SSH_AUTH_SOCK = bitwardenSshAuthSock;
 
+  # GUIログイン時に常駐し、Emacs Clientから同じデーモンへ接続する。
+  # パッケージは programs.emacs の固定済みEmacsを引き継ぐ。
+  services.emacs = {
+    enable = true;
+    startWithUserSession = "graphical";
+    client.enable = true;
+  };
+
+  # デスクトップが設定した別のエージェントを継承しないよう明示する。
+  systemd.user.services.emacs.Service.Environment = [
+    "SSH_AUTH_SOCK=${bitwardenSshAuthSock}"
+  ];
+
   # デスクトップセッションが別のSSHエージェントを設定した場合も、
   # 対話シェルではBitwardenを使う。sessionVariablesの再読み込みに依存しない。
   programs.bash.initExtra = lib.mkAfter bitwardenSshAgentInit;
